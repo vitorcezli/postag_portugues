4f852a461ea9cc37bf609f87c58107342b8856e8
Vítor Cézar <vitorcezli@gmail.com>
2018-11-26 09:04:46 -0200

    Divisão de dados está pronta.

--- leitor_postag.py
+++ leitor_postag.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
-from conversor_postag import converte_palavra_lista
 import codecs
+import math
+import random
+
+from conversor_postag import converte_palavra_lista
 
 
 def __separa_palavras_tags(palavras_tags):
@@ -8,6 +10,56 @@ def __separa_palavras_tags(palavras_tags):
 	palavras = [pt[: pt.index('_')] for pt in palavras_tags]
 	tags = [pt[pt.index('_') + 1 :] for pt in palavras_tags]
 	return [palavras, tags]
+
+
+def __divide_subsets(valores_set, tamanho):
+	if len(valores_set) <= tamanho:
+		return [(v,) for v in valores_set]
+	tamanho_set = len(valores_set) // tamanho
+	sets_retorno = list()
+	for i in range(tamanho):
+		if i == tamanho - 1:
+			sets_retorno.append(tuple(valores_set))
+			break
+		set_criado = set(random.sample(valores_set, tamanho_set))
+		sets_retorno.append(tuple(set_criado))
+		valores_set -= set_criado
+	return sets_retorno
+
+
+def __divide_sets(arquivo, tamanho_divisao):
+	linhas = codecs.open(arquivo, 'r').read().split('\n')[:-1]
+	tags_dict = dict()
+	for linha in linhas:
+		palavras, tags = __separa_palavras_tags(linha.split())
+		for palavra, tag in zip(palavras, tags):
+			tags_dict[tag] = tags_dict.get(tag, [])
+			tags_dict[tag].append(palavra)
+	sets_dict = dict()
+	for key in tags_dict:
+		if len(set(tags_dict[key])) > tamanho_divisao:
+			sets_dict[key] = __divide_subsets(set(tags_dict[key]), tamanho_divisao)
+	return sets_dict
+
+
+def treinamento_validacao(arquivo, tamanho_divisao, numeracao):
+	treinamento = list()
+	validacao = list()
+	sets_dict = __divide_sets(arquivo, tamanho_divisao)
+	linhas = codecs.open(arquivo, 'r').read().split('\n')[:-1]
+	for linha in linhas:
+		palavras, tags = __separa_palavras_tags(linha.split())
+		se_validacao = False
+		for palavra, tag in zip(palavras, tags):
+			if tag not in sets_dict:
+				continue
+			if palavra in sets_dict[tag][numeracao]:
+				validacao.append(linha)
+				se_validacao = True
+				break
+		if not se_validacao:
+			treinamento.append(linha)
+	return treinamento, validacao
 
 
 def leia_palavras_postags(arquivo):
@@ -21,3 +73,10 @@ def leia_postag_por_palavra(arquivo):
 	linhas = codecs.open(arquivo, 'r').read().split('\n')[:-1]
 	pt = set([(c.split('_')[0], c.split('_')[1]) for l in linhas for c in l.split()])
 	return [converte_palavra_lista(c[0], 6) + [c[1]] for c in pt]
+
+
+if __name__ == '__main__':
+	__divide_sets('data.txt', 10)
+	for i in range(10):
+		treinamento, validacao = treinamento_validacao('data.txt', 10, i)
+		print(len(treinamento), len(validacao))
--- treinamento_postag.py
+++ treinamento_postag.py
@@ -7,44 +7,42 @@ import numpy as np
 import pickle
 
 
-# lê as palavras e as classificações do arquivo
-lista_palavras_tags = leia_palavras_postags('macmorpho-train.txt')
-# variáveis utilizadas para armazenar dados que serão utilizados para treinar
-# o classificador por palavras e executar o algoritmo Viterbi
-observacoes = {}
-bigrams = {}
-unigrams = {}
-# armazena os dados
-for linha in lista_palavras_tags:
-	palavras = linha[0]
-	tags = linha[1]
-	# armazena as observações
-	for i in range(len(tags)):
-		observacoes[tags[i], palavras[i]] = observacoes.get((tags[i], palavras[i]), 0) + 1
-	# armazena unigrams
-	unigrams['<s>'] = unigrams.get('<s>', 0) + 1
-	for i in range(len(tags)):
-		unigrams[tags[i]] = unigrams.get(tags[i], 0) + 1
-	# armazena bigrams
-	bigrams['<s>', tags[0]] = bigrams.get(('<s>', tags[0]), 0) + 1
-	for i in range(1, len(tags)):
-		bigrams[tags[i - 1], tags[i]] = bigrams.get((tags[i - 1], tags[i]), 0) + 1
-	bigrams[tags[len(tags) - 1], '</s>'] = bigrams.get((tags[len(tags) - 1], '</s>'), 0) + 1
-# lê as palavras do corpus em forma de lista e com a classificação de
-# postag na última coluna
-palavras_tags = leia_postag_por_palavra('macmorpho-train.txt')
-# a variável tags é utilizada para indexar as classificações
-tags = list(set(linha[-1] for linha in palavras_tags))
-# as árvores de decisão de Random Forest necessitam de valores numéricos
-# nos atributos e nas classificações
-data_x = [[ord(c) for c in linha[: -1]] for linha in palavras_tags]
-data_y = [tags.index(linha[-1]) for linha in palavras_tags]
-# treina o classificador de RandomForest
-classificador = RandomForestClassifier()
-classificador.fit(data_x, data_y)
-# salva os dados do classificador e das informações para o algoritmo Viterbi
-pickle.dump(classificador, open('classificador.sav', 'wb'))
-pickle.dump(observacoes, open('observacao.sav', 'wb'))
-pickle.dump(unigrams, open('unigram.sav', 'wb'))
-pickle.dump(bigrams, open('bigram.sav', 'wb'))
-pickle.dump(tags, open('tags.sav', 'wb'))
+def treina_hmm():
+	# lê as palavras e as classificações do arquivo
+	lista_palavras_tags = leia_palavras_postags('data.txt')
+	# variáveis utilizadas para armazenar dados que serão utilizados para treinar
+	# o classificador por palavras e executar o algoritmo Viterbi
+	observacoes = {}
+	bigrams = {}
+	unigrams = {}
+	# armazena os dados
+	for linha in lista_palavras_tags:
+		palavras = linha[0]
+		tags = linha[1]
+		# armazena as observações
+		for i in range(len(tags)):
+			observacoes[tags[i], palavras[i]] = observacoes.get((tags[i], palavras[i]), 0) + 1
+		# armazena unigrams
+		unigrams['<s>'] = unigrams.get('<s>', 0) + 1
+		for i in range(len(tags)):
+			unigrams[tags[i]] = unigrams.get(tags[i], 0) + 1
+		# armazena bigrams
+		bigrams['<s>', tags[0]] = bigrams.get(('<s>', tags[0]), 0) + 1
+		for i in range(1, len(tags)):
+			bigrams[tags[i - 1], tags[i]] = bigrams.get((tags[i - 1], tags[i]), 0) + 1
+		bigrams[tags[len(tags) - 1], '</s>'] = bigrams.get((tags[len(tags) - 1], '</s>'), 0) + 1
+	# lê as palavras do corpus em forma de lista e com a classificação de
+	# postag na última coluna
+	palavras_tags = leia_postag_por_palavra('macmorpho-train.txt')
+	# a variável tags é utilizada para indexar as classificações
+	tags = list(set(linha[-1] for linha in palavras_tags))
+	# as árvores de decisão de Random Forest necessitam de valores numéricos
+	# nos atributos e nas classificações
+	data_x = [[ord(c) for c in linha[: -1]] for linha in palavras_tags]
+	data_y = [tags.index(linha[-1]) for linha in palavras_tags]
+	# treina o classificador de RandomForest
+	classificador = RandomForestClassifier()
+	classificador.fit(data_x, data_y)
+	# salva os dados do classificador e das informações para o algoritmo Viterbi
+	return [classificador, observacoes, unigrams, bigrams, tags]
+
